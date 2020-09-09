42f7e3b06ea06f96a794104ae558f1728177a80a
Jacob Trueb <jtrueb@northwestern.edu>
2020-09-09 14:16:12 -0500

    Add poll option to receive output

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use actix::{Actor, StreamHandler};
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
 use actix_web_actors::ws;
+use actix_web_actors::ws::WebsocketContext;
+use std::str;
 
 enum CliError {
     ZmqError(zmq::Error),
@@ -18,27 +20,47 @@ impl std::convert::From<CliError> for actix_web::Error {
     }
 }
 
-/// Define http actor
+/// Define http actor with connected zmq socket (and context)
 struct WsZmqActor {
-    zmq_publisher: zmq::Socket,
+    zmq_req_dealer: zmq::Socket,
     _zmq_ctx: zmq::Context,
 }
 
 impl WsZmqActor {
     pub fn new(endpoint: &str) -> Result<WsZmqActor, CliError> {
         let ctx = zmq::Context::new();
-        let publisher = ctx.socket(zmq::PUB)?;
-        publisher.connect(endpoint)?;
+
+        let req_dealer= ctx.socket(zmq::DEALER)?;
+        req_dealer.connect(endpoint)?;
 
         Ok(WsZmqActor {
-            zmq_publisher: publisher,
+            zmq_req_dealer: req_dealer,
             _zmq_ctx: ctx,
         })
+    }
+
+    fn _forward_message(&mut self, request_string: &str) -> Result<(), zmq::Error> {
+        // Send the request
+        self.zmq_req_dealer.send(&vec![], zmq::SNDMORE)?;        // Simulated REQ: Empty Frame
+        self.zmq_req_dealer.send(request_string.as_bytes(), 0)?; // Simulated REQ: Message Content
+        Ok(())
+    }
+
+    fn _receive_message(&mut self) -> Result<Vec<u8>, zmq::Error> {
+        // Send the request
+        if self.zmq_req_dealer.poll(zmq::POLLIN, 10000)? == 0 {
+            return Ok(vec![]);
+        }
+
+        let _ = self.zmq_req_dealer.recv_bytes(0)?;                    // Simulated REQ: Empty Frame
+        let msg = self.zmq_req_dealer.recv_bytes(0)?;         // Simulated REQ: Message Content
+        return Ok(msg)
+
     }
 }
 
 impl Actor for WsZmqActor {
-    type Context = ws::WebsocketContext<Self>;
+    type Context = WebsocketContext<Self>;
 }
 
 /// Handler for ws::Message message
@@ -52,9 +74,39 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WsZmqActor {
             Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
             Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
             Ok(ws::Message::Text(text)) => {
-                println!("Text: {:#?}", text);
-                self.zmq_publisher.send(text.as_bytes(), 0).unwrap();
-                ctx.text(text);
+                // Try find response
+                if text == "poll" {
+                    // Request is sent, Wait for the Response
+                    let resp = match self._receive_message() {
+                        Ok(msg) => {
+                            msg
+                        },
+                        Err(err) => {
+                            println!("Failed to receive response to foward: {}", err.to_string());
+                            return;
+                        }
+                    };
+
+                    // Send the response if there was one
+                    println!("Found response: {}", String::from(str::from_utf8(resp.as_slice()).unwrap_or("!!! E R R O R !!!")));
+                    if resp.len() > 0 {
+                        let string = String::from_utf8(resp);
+                        if string.is_ok() {
+                            ctx.text(string.unwrap().as_str());
+                        }
+                    }
+                } else {
+                    // Send the message
+                    println!("Found Request: {}", text);
+                    match self._forward_message(text.as_str()) {
+                        Ok(_) => { },
+                        Err(err) => {
+                            // Swallowing ZMQ forwarding error
+                            println!("Failed to forward: {} due to: {}", &text, err);
+                            return;
+                        }
+                    }
+                }
             }
             _ => (),
         }
@@ -62,7 +114,7 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WsZmqActor {
 }
 
 async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, actix_web::Error> {
-    let connector = match WsZmqActor::new("tcp://ubuntu20:5555") {
+    let connector = match WsZmqActor::new("tcp://ubuntu20:6000") {
         Ok(connector) => connector,
         Err(err) => {
             return Err(std::convert::From::from(err));
@@ -75,22 +127,7 @@ async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, a
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    // Run a ZMQ subscriber on one thread
-    std::thread::spawn(|| {
-        let ctx = zmq::Context::new();
-        let subscriber = ctx.socket(zmq::SUB).unwrap();
-        subscriber.connect("tcp://ubuntu20:5556").unwrap();
-        subscriber.set_subscribe(&vec![]).unwrap();
-
-        println!("Subscribed!");
-
-        loop {
-            let message= subscriber.recv_string(0).unwrap().unwrap();
-            println!("{}", message);
-        }
-    });
-
-    // Run a WS server on one thread
+    // Run a WS server
     HttpServer::new(|| App::new().route("/ws/", web::get().to(index)))
         .bind("127.0.0.1:8088")?
         .run()
